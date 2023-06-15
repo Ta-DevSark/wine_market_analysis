cc50b6b1f056b02e51e05df2d32956675161599b
Tad <tadevosian.sarkis@gmail.com>
2023-06-15 17:37:23 +0200

    Final product.

--- app.py
+++ app.py
@@ -1,48 +1,48 @@
 import sqlite3 as sq3
-import pandas as pd
 import streamlit as st
-import matplotlib as plt
 
-conn = sq3.connect('vivino.db')
+# Set page config and title
+st.set_page_config(page_title="Vivino Wine Data", layout="wide")
 
-st.write("""
-# Vivino, great data on the greatest wines
-
-It's all about wine !
-
-""")
-
-# Create a database connection
+# Connect to the database
 conn = sq3.connect('vivino.db')
 
 # Create a cursor object to execute SQL queries
 cursor = conn.cursor()
 
 # Read SQL files and execute the queries
-sql_files = ['queries/best_winery.sql', 
-             'queries/list10.sql', 
-             'queries/marketing.sql',
-             'queries/taste_keywords.sql',
-             'queries/top3.sql',
-             'queries/vintage_leaderboard.sql',
-             'queries/wine_leaderboard.sql',
-             'queries/price.sql']
+sql_files = [
+    'queries/best_winery.sql',
+    'queries/list10.sql',
+    'queries/marketing.sql',
+    'queries/taste_keywords.sql',
+    'queries/top3.sql',
+    'queries/vintage_leaderboard.sql',
+    'queries/wine_leaderboard.sql'
+]
 
-# Function to run SQL file and return the result as a DataFrame
+# Function to run SQL file and return the result as a list of tuples
 def run_sql_file(file, cursor):
     with open(file, 'r') as sql_file:
         sql_query = sql_file.read()
         cursor.execute(sql_query)
         result = cursor.fetchall()
-        column_names = [description[0] for description in cursor.description]
-        df = pd.DataFrame(result, columns=column_names)
-        return df
+        return result
+    
+# Function to convert the result to a table
+def convert_to_table(result, cursor):
+    column_names = [description[0] for description in cursor.description]
+    table = []
+    table.append(column_names)
+    table.extend(result)
+    return table
 
 # Display the tables for each SQL file
 for file in sql_files:
-    st.write(f"## Results for {file[8:-4]}")
-    df = run_sql_file(file, cursor)
-    st.dataframe(df)
+    st.write(f"Results for {file[8:-4]}")
+    result = run_sql_file(file, cursor)
+    table = convert_to_table(result, cursor)
+    st.table(table)
 
 # Close the connection
 conn.close()
--- queries/taste_keywords.sql
+++ queries/taste_keywords.sql
@@ -6,5 +6,10 @@ SELECT keywords.id AS key_id,
 FROM keywords
 JOIN keywords_wine ON keywords.id = keywords_wine.keyword_id
 JOIN wines ON keywords_wine.wine_id = wines.id
-WHERE keywords.name IN ('Coffee', 'Toast', 'Green apple', 'cream', 'citrus')
-  AND keywords_wine.count > 10;
+WHERE keywords.name IN ('coffee', 'toast', 'green apple', 'cream', 'citrus')
+ AND keyword_type = "primary"
+ AND keywords_wine.count > 10
+
+GROUP BY wines.name
+
+ORDER BY keywine_count DESC;
