6265f205e70195350817b15cd61cdda1402cead7
Ta-DevSark <tadevosian.sarkis@gmail.com>
2023-06-16 09:13:35 +0200

    Starting to look like a website

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import sqlite3 as sq3
 import streamlit as st
 
 # Set page config and title
-st.set_page_config(page_title="Vivino Wine Data", layout="wide")
+st.set_page_config(page_title="Vivino Wine Data", page_icon=":wine_glass:", layout="wide")
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.title("Wiwino :wine_glass:")
+    st.subheader("Great data on the greatest wines")
 
 # Connect to the database
 conn = sq3.connect('vivino.db')
@@ -44,6 +49,28 @@ for file in sql_files:
     result = run_sql_file(file, cursor)
     table = convert_to_table(result, cursor)
     st.table(table)
+
+# ---- CONTACT ----
+with st.container():
+    st.write("---")
+    st.header("Contact us !")
+    st.write("##")
+
+    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
+    contact_form = """
+    <form action="https://formsubmit.co/YOUR@MAIL.COM" method="POST">
+        <input type="hidden" name="_captcha" value="false">
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
+        <textarea name="message" placeholder="Your message here" required></textarea>
+        <button type="submit">Send</button>
+    </form>
+    """
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.markdown(contact_form, unsafe_allow_html=True)
+    with right_column:
+        st.empty()
 
 # Close the connection
 conn.close()
